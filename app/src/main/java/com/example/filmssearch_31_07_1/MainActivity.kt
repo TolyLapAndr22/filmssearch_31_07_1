df46c01ddf0093b76d0f04ded3514e8a83d55bfd
aalapandr22 <aalap73@mail.ru>
2022-08-01 18:28:50 +0300

    modul_19_9

---
+++ app/src/main/java/com/example/filmssearch_31_07_1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.filmssearch_31_07_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/filmssearch_31_07_1/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.filmssearch_31_07_1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
